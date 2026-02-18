05afd7cce066c545716fd96053bc887029436eeb
rchmeis <r.chmeis@gmail.com>
2026-02-18 21:49:26 +0100

    Add project files.

---
+++ DNA/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace DNA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input first DNA sequence: ");
+            string dna1 = Console.ReadLine().ToLower();
+            Console.WriteLine("Inpute second DNA sequence: ");
+            string dna2 = Console.ReadLine().ToLower();
+
+            Dna dna = new Dna();
+            //This method adds difference in length to the number of differing letters.
+            int hammingDistance = dna.CompareDna(dna1, dna2);
+            Console.WriteLine($"The hamming-distance is {hammingDistance}");
+        }
+    }
+}

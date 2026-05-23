d8858b6a44f8467d1effeeb526b7b0ae0be37460
rchmeis <r.chmeis@gmail.com>
2026-05-23 13:16:01 +0200

    Code cleanup

---
+++ DNA/DnaCalculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace DNA
+{
+    internal class DnaCalculator
+    {
+        public int HammingDistance(string dna1, string dna2)
+        {
+            int diff = 0;
+            int minLength = Math.Min(dna1.Length, dna2.Length);
+            int lengthDiff = Math.Max(dna1.Length, dna2.Length) - minLength;
+
+            for (int i = 0; i < minLength;i++)
+            {
+                if (dna1[i] != dna2[i])
+                {
+                    diff += 1;
+                }
+            }
+            
+            int hammingDistance = diff + lengthDiff;
+            return hammingDistance;
+        }
+    }
+}
--- DNA/Program.cs
+++ DNA/Program.cs
@@ -3,16 +3,19 @@
     internal class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Input first DNA sequence: ");
-            string dna1 = Console.ReadLine().ToLower();
-            Console.WriteLine("Inpute second DNA sequence: ");
-            string dna2 = Console.ReadLine().ToLower();
+        {        
 
-            Dna dna = new Dna();
-            //This method adds difference in length to the number of differing letters.
-            int hammingDistance = dna.CompareDna(dna1, dna2);
-            Console.WriteLine($"The hamming-distance is {hammingDistance}");
+            Console.Write("indtast DNA1");
+            string DNA1 = Console.ReadLine().ToUpper();
+            Console.Write("indtast DNA2");
+            string DNA2 = Console.ReadLine().ToUpper();
+
+            DnaCalculator calculator = new DnaCalculator();
+            Console.WriteLine($"\n\nThe hamming distance is {calculator.HammingDistance(DNA1, DNA2)}");
+
+
+
+
         }
     }
 }
